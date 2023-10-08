062d86e7e31c26d2f218ad56cb662e44a6e919a6
leew0nseokgit config --global user.name leew0nseokgit config --global user.name leew0nseok <zzang50aa@gmail.com>
2023-10-08 15:53:12 +0900

    ch5 오차역전파법

--- 밑시딥_구현/common/layers.py
+++ 밑시딥_구현/common/layers.py
@@ -54,7 +54,7 @@ class Affine:
         x = x.reshape(x.shape[0], -1)
         self.x = x
 
-        out = np.dot(self.x, self.W) + self.b
+        out = np.dot(x, self.W) + self.b
 
         return out
 
